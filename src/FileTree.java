531e4721713e7b63639621251fe9131230c27928
Tristin-Young <86166175+Tristin-Young@users.noreply.github.com>
2023-10-20 08:10:37 -0400

    Added comments for easier readability

--- src/FileTree.java
+++ src/FileTree.java
@@ -1,7 +1,16 @@
+//Author: Tristin Young
+
+//importing necessary packages
 import java.io.File;
 import java.util.Scanner;
 
+
+//fileTree class
 public class FileTree {
+
+    //buildTree method
+    //takes in initial file path and verifies that it is a directory
+    //if it is a directory, it calls recBuildTree method
     public static TreeNode buildTree(String path) {
         File root = new File(path);
         if (!root.isDirectory() || !root.exists()) {
@@ -10,7 +19,9 @@ public class FileTree {
         return recBuildTree(root);
     }
 
+    //recBuildTree method
     private static TreeNode recBuildTree(File directory) {
+        //takes in a file and creates a TreeNode for it
         TreeNode folder = new TreeNode(directory.getName());
 
         File[] files = directory.listFiles();
@@ -18,12 +29,15 @@ public class FileTree {
             return folder;
         } else {
             for (File file : files) {
+                //ignores hidden files
                 if (!file.getName().startsWith(".")) {
+                    //if the file is a directory, it recursively calls itself
                     if (file.isDirectory()) {
                         TreeNode subFolder = recBuildTree(file);
                         if (subFolder.getNumFiles() > 0 || subFolder.getChildNodes().size() > 0) {
                             folder.addChildNode(subFolder);
                         }
+                    //if the file is a file, it increments the file count and size
                     } else {
                         folder.incrementFileCount();
                         folder.incrementSize(file.length());
@@ -32,31 +46,42 @@ public class FileTree {
                 }
             }
         }
+        //returns the TreeNode
         return folder;
     }
 
+    //printTree method
     private static void printTree(TreeNode root, int depth) {
+
+        //handles spacing of tree structure
         for (int i = 0; i < depth; i++) {
             System.out.print("  ");
         }
+        //printing for folders
         System.out.println(">" + root.getFolderName() + " (" + root.getNumFiles() + " files, " + root.getSizeFiles() + " bytes)");
+        //handles spacing of tree structure
         for (String fileName : root.getFilesInFolder()) {
             for (int i = 0; i < depth; i++) {
                 System.out.print("  ");
             }
+            //printing for files
             System.out.println("  --" + fileName);
         }
+        //recursively calls itself for each child node
         for (TreeNode child : root.getChildNodes()) {
             printTree(child, depth + 1);
         }
     }
 
+    //main method
     public static void main(String[] args) {
+        //takes in user input for file path
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter path to directory: ");
         String path = sc.nextLine();
         sc.close();
 
+        //calls buildTree method and prints the tree
         TreeNode root = buildTree(path);
         if (root == null) {
             System.out.println("Invalid path");
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,6 +1,8 @@
+//imprting packages
 import java.util.ArrayList;
 import java.util.List;
 
+//TreeNode class
 public class TreeNode {
     private String folderName;
     private int numFiles;
@@ -8,6 +10,7 @@ public class TreeNode {
     private List<TreeNode> childNodes;
     private List<String> filesInFolder;
 
+    //constructor
     public TreeNode(String folderName) {
         this.folderName = folderName;
         this.numFiles = 0;
@@ -16,38 +19,39 @@ public class TreeNode {
         this.filesInFolder = new ArrayList<>();
     }
 
+    //adding child nodes
     public void addChildNode(TreeNode childNode) {
         this.childNodes.add(childNode);
     }
-
+    //incrementing file count
     public void incrementFileCount() {
         this.numFiles++;
     }
-
+    //incrementing size
     public void incrementSize(long size) {
         this.sizeFiles += size;
     }
-
+    //adding files
     public void addFile(String fileName) {
         this.filesInFolder.add(fileName);
     }
-
+    //getting folder name
     public String getFolderName() {
         return this.folderName;
     }
-
+    //getting file count
     public int getNumFiles() {
         return this.numFiles;
     }
-
+    //getting size
     public long getSizeFiles() {
         return this.sizeFiles;
     }
-
+    //getting child nodes
     public List<TreeNode> getChildNodes() {
         return this.childNodes;
     }
-
+    //getting files
     public List<String> getFilesInFolder() {
         return this.filesInFolder;
     }
